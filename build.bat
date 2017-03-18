14123c017c33d554d869d78594352ded942501a0
Jason Darby <jasonkdarby@gmail.com>
2017-03-17 22:42:47 -0400

    cleaned up output from batch files

--- build.bat
+++ build.bat
@@ -1,2 +1,5 @@
-echo 'This will take a lot of time...'
+@echo off
+
+echo This will take a lot of time...
+
 docker build -t jasonkdarby/docker-cursive-ide .
--- start-intellij.bat
+++ start-intellij.bat
@@ -1,8 +1,14 @@
-docker run ^
--tdi ^
--e DISPLAY=%DISPLAY% ^
---net=host ^
---pid=host ^
---ipc=host ^
--v %HOMEDRIVE%%HOMEPATH%/.IdeaIC2016.3_docker:/home/developer/.IdeaIC2016.3 ^
-jasonkdarby/docker-cursive-ide
+@echo off
+
+if defined DISPLAY (
+    docker run ^
+    -tdi ^
+    -e DISPLAY=%DISPLAY% ^
+    --net=host ^
+    --pid=host ^
+    --ipc=host ^
+    -v %HOMEDRIVE%%HOMEPATH%/.IdeaIC2016.3_docker:/home/developer/.IdeaIC2016.3 ^
+    jasonkdarby/docker-cursive-ide
+) else (
+    echo DISPLAY is not set in the environment.
+)
